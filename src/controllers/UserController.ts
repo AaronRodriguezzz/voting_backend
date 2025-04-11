07dbc7a6cd183b33adb1a940e1a8eb35d18491d5
lindsey5 <lindseysamson5@gmail.com>
2025-04-12 02:02:17 +0800

    add comments

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,22 +4,26 @@ import { errorHandler } from '../utils/errorHandler';
 
 export const createUser = async (req: Request, res: Response) => {
     try {
+        // Check if the email or studentID is already in use by querying the database
         const isExist = await userService.findUser({
             $or: [
                 {email: req.body.email},
                 {studentID: req.body.studentID},
             ]
         })
-
+        // If either the email or studentID is already used, throw an error
         if(isExist){
             throw new Error('The email or studentID is already registered')
         }
-
+        // Create a new user by calling the createUser service method and save it to the database
         const newUser = await userService.createUser(req.body);
+         // Respond with a 201 status and the newly created user object
         res.status(201).json(newUser);
 
     } catch (error) {
+         // Use the errorHandler utility to format the error messages
         const errors = errorHandler(error as Error);
+        // Respond with a 400 status and the error messages
         res.status(400).json({errors});
     }
 };
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,17 +1,19 @@
 import { FilterQuery } from "mongoose";
 import User, { IUser } from "../models/User"
 
+// Service to create a new user and save it to the database
 export const createUser = async (userData : any) => {
-    const newUser = new User(userData)
-    return await newUser.save();
+    const newUser = new User(userData); // Create a new user instance
+    return await newUser.save(); // Save the new user to the database
 }
 
+// Service to find a user based on the query and exclude the password field
 export const findUser = async (query: FilterQuery<IUser>) => {
     const user = await User.findOne(query).exec();
     if (user) {
-        const { password, ...userWithoutPassword } = user.toObject();
-        return userWithoutPassword;
+        const { password, ...userWithoutPassword } = user.toObject(); // Remove the password field
+        return userWithoutPassword; // Return the user object without the password
     }
 
-    return null;
+    return null; // Return null if no user is found
 };
